7e729f4919ea8d39d249daf07397cfd0cf50acc1
support <support@PC-8CG112DHBW.ad.jotunserver.com>
2024-09-15 13:12:06 +0200

    feat: Add main menu and pause

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -1,28 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadLevel : MonoBehaviour
 {
     public string LevelName;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Application.LoadLevel(LevelName);
+            SceneManager.LoadScene(LevelName);
         }
     }
 }
